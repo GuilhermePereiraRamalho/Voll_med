59e5428a740c7d44f268452aa53a00f8130c5ce8
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2023-11-01 22:05:36 +0100

    adicionando campo de telefone no cadastro

--- api/api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ api/api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -11,16 +11,22 @@ public record DadosCadastroMedico(
 
         @NotBlank
         String nome,
+
         @NotBlank
         @Email
         String email,
+
+        @NotBlank
+        String telefone,
+
         @NotBlank
         @Pattern(regexp = "\\d{4,6}")
         String crm,
+
         @NotNull
         Especialidade especialidade,
+
         @NotNull
         @Valid
         DadosEndereco endereco) {
-
 }
