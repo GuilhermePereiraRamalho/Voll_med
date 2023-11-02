c188af150a9e638492da1900d5a5eb6f87360160
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2023-11-02 16:18:23 +0100

    Criando o push e delete para pacientes

--- api/api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,10 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.paciente.DadosCadastroPaciente;
-import med.voll.api.paciente.DadosListagemPacientes;
-import med.voll.api.paciente.Paciente;
-import med.voll.api.paciente.PacienteRepository;
+import med.voll.api.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -26,8 +23,22 @@ public class PacienteController {
     }
 
     @GetMapping
-    public Page<DadosListagemPacientes> lista(@PageableDefault(page = 0, size = 10, sort = {"nome"})Pageable paginacao){
-        return repository.findAll(paginacao).map(DadosListagemPacientes::new);
+    public Page<DadosListagemPacientes> lista(@PageableDefault(page = 0, size = 10, sort = {"nome"}) Pageable paginacao){
+        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemPacientes::new);
+    }
+
+    @PutMapping
+    @Transactional
+    public void atulizar(@RequestBody @Valid DadosAtualizacaoPaciente dados){
+        var paciente = repository.getReferenceById(dados.id());
+        paciente.atualizarInformacoes(dados);
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void remover(@PathVariable Long id){
+        var paciente = repository.getReferenceById(id);
+        paciente.inativar();
     }
 
 }
