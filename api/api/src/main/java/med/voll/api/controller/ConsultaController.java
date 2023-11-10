dd2c5dfeb42135cc6df755e4083c8dee35bfd57e
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2023-11-10 08:45:19 +0100

    Update ConsultaController.java

--- api/api/src/main/java/med/voll/api/controller/ConsultaController.java
+++ api/api/src/main/java/med/voll/api/controller/ConsultaController.java
@@ -2,6 +2,8 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("consultas")
 public class ConsultaController {
 
+    @PostMapping
+    @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados){
         System.out.println(dados);
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
